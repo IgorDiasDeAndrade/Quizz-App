5e202d1e5cd727ffe1949b1868ccbd2110e127a5
IgorDiasdeAndrade <igor.d.andrade@live.com>
2022-06-12 16:52:47 -0300

    Gerado controller, rotas,, views e actions para questions, adicionada exibicao de questions juntamente de subjects

---
+++ app/controllers/admins_backoffice/questions_controller.rb
@@ -0,0 +1,58 @@
+class AdminsBackoffice::QuestionsController < AdminsBackofficeController
+    before_action :set_question, only: [:edit, :update, :destroy]
+    before_action :get_subjects, only: [:new, :edit]
+  
+    def index
+      @questions = Question.includes(:subject)
+                           .order(:description)
+                           .page(params[:page]).per(10)
+    end
+  
+    def new
+      @question = Question.new
+    end
+  
+    def create
+      @question = Question.new(question_params)
+      if @question.save()
+        redirect_to admins_backoffice_questions_path, notice: "Criado"
+      else
+        render :new
+      end
+    end
+  
+  
+    def edit
+    end
+  
+    def update
+      if @question.update(question_params)
+        redirect_to admins_backoffice_questions_path, notice: "Assunto cadastrado com sucesso!"
+      else
+        render :edit
+      end
+    end
+  
+    def destroy
+      if @question.destroy
+        redirect_to admins_backoffice_questions_path, notice: "excluido com sucesso!"
+      else
+        render :index
+      end
+    end
+  
+  
+    private
+  
+    def set_question
+      @question = Question.find(params[:id])
+    end
+  
+    def question_params
+      question_params = params.require(:question).permit(:description, :subject_id)
+    end
+      
+    def get_subjects
+        @subjects = Subject.all
+    end
+end
