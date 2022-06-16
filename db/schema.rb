e32e4a5be42d58ddcafaa5e56f4473c84a778799
IgorDiasdeAndrade <igor.d.andrade@live.com>
2022-06-16 12:00:08 -0300

    Adicionada condição para escolha de layout quando estiver no controller do devise e adicionado layout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_06_10_183830) do
-  create_table "admins", charset: "utf8mb4", force: :cascade do |t|
+  create_table "admins", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_10_183830) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
-  create_table "answers", charset: "utf8mb4", force: :cascade do |t|
+  create_table "answers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "description", null: false
     t.bigint "question_id", null: false
     t.boolean "correct", default: false
@@ -32,7 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_10_183830) do
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
-  create_table "questions", charset: "utf8mb4", force: :cascade do |t|
+  create_table "questions", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "description", null: false
     t.bigint "subject_id", null: false
     t.datetime "created_at", null: false
@@ -40,13 +40,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_10_183830) do
     t.index ["subject_id"], name: "index_questions_on_subject_id"
   end
 
-  create_table "subjects", charset: "utf8mb4", force: :cascade do |t|
+  create_table "subjects", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
