58a211448fde3440719ee1db2676fd5097c62961
igordiasdeandrade <igor.d.andrade@live.com>
2022-07-14 23:11:27 +0000

    Adicionado padronização para view de recuperação de senha de email, adicionado mailtrap para testes de email e criada variavel global de params para solucionar problema de validação condicional

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,14 @@ class User < ApplicationRecord
   accepts_nested_attributes_for :user_profile, reject_if: :all_blank
   
   # Validations
-  validates :first_name, presence: true, length: {minimum: 3}, on: :update
+  validates :first_name, presence: true, length: {minimum: 3}, on: :update, unless :reset_password_token_present?
 
 
   def full_name
     [self.first_name, self.last_name].join(' ')
+  end
+
+  def reset_password_token_present
+    !!$global_params[:user][:password_token]
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,17 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => '3fe1990874d920',
+    :password => 'f5bc6ddad07d30',
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
